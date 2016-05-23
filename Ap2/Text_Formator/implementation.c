fbab47a54880b96f093922d264ff663a4afc7b8b
bornia <guilhermeborniamiranda@gmail.com>
2016-05-23 18:54:29 -0300

    Ajustes no Main

--- Ap2/Text_Formator/implementation.c
+++ Ap2/Text_Formator/implementation.c
@@ -5,7 +5,7 @@
 // **| IMPLEMENTATION |**
 void getting_uppercase ( char *name )
 {
-	int i;	
+	int i;
 
 	for ( i = 0; i < strlen(name); i++ )
 		if ( ( islower ( name[i+1] ) != 0 ) && ( name[i] == ' ' ) )	//islower return 0 if it's not a lowercase letter
@@ -23,7 +23,12 @@ void realocating_spaces ( char *name )
 			name[i] = name[i+1];	// Put all the letters in theirs previous position
 			for ( j = i+1; j < strlen(name); j++ )	// Delete the same letters generated for the substitutions
 				name[j] = name[j+1];		// of spaces
-				
 		}
 	}
+}
+
+void getting_lowercase ( char *name )
+{
+    if ( islower ( name[0] ) != 0 )	// iF it is lowercase, returns a value different of zero
+    name[0] = toupper ( name[0] );	// If all spaces were removed, the first position, definitly, is a letter.
 }

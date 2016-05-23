fcff141f0133baca8309702d06c5ad12c30b615c
bornia <guilhermeborniamiranda@gmail.com>
2016-05-23 11:29:53 -0300

    Quarta Versão

--- Código do Sistema/Text_Formator/main.c
+++ Código do Sistema/Text_Formator/main.c
@@ -10,21 +10,59 @@
 #include "source.h"
 // **| MAIN |**
 int main ()
-{
-	char name[50];	// It is going to keep the name of my user
-	
-	printf ("\n Type your name:\n >> ");
-	fgets ( name, 50, stdin );
+{   // It is going to keep the name of my user;
+	char name[600]; // 600 characters divided by 6 characters (one word on average) allows the user to digit 100 words approximately
+	int opcao = 0, opcao1 = 0, opcao2 = 0,  // will be filled by the user
+        aux = 0, aux1 = 0, aux2 = 0;    // used to control the menus
 
-	getting_uppercase ( name );
-	realocating_spaces ( name );
+	do
+    {
+        if (aux1 == 0) // Allows that be impress once
+        {
+            do
+            {
+                system("Clear");
+                printf("\n Do you want to start the program?\n (1) Yes\n (2) No\n >> ");
+                scanf("%d", &opcao);
+                aux1++;
+                if (opcao == 2)
+                    return 0;
+            } while (opcao != 1);
+        }
 
-	if ( islower ( name[0] ) != 0 )	// iF it is lowercase, returns a value different of zero
-		name[0] = toupper ( name[0] );	// If all spaces were removed, the first position, definitly, is a letter.
-						
-	printf ("\n Your name is:\n%s\n", name);
+        system("Clear");
+        printf ("\n Type your name:\n >> ");
+        fgets ( name, 50, stdin );  fgets ( name, 50, stdin );
 
-	return 0;
+        do
+        {
+            system("Clear");
+            printf("\n Do you want to format the text?\n (1) Yes\n (2) No\n >> ");
+            scanf("%d", &opcao1);
+            if (opcao1 == 1)
+            {
+                getting_uppercase ( name );
+                realocating_spaces ( name );
+                getting_lowercase ( name );
+            }
+        } while ( opcao1 != 1 && opcao1 != 2);
+
+        system("Clear");
+        printf ("\n Your name is:\n%s\n\n", name);
+        system("Pause");
+
+        do
+        {
+            system("Clear");
+            printf("\n Do you want to type another text?\n (1) Yes\n (2) No\n >> ");
+            scanf("%d", &opcao2);
+            if (opcao2 == 2)
+                aux++;
+        } while ( opcao2 != 1 && opcao2 != 2 );
+
+    } while ( aux == 0 );
+
+    return 1;
 }
 
 
--- Código do Sistema/Text_Formator/source.h
+++ Código do Sistema/Text_Formator/source.h
@@ -7,4 +7,5 @@
 // **| PROTOTYPES |**
 void getting_uppercase ( char *name );
 void realocating_spaces ( char *name );
+void getting_lowercase ( char *name );
 
